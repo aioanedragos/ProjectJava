f9aadb40a16040ef3d085f38a506bfa312ebf8e8
Dragos Aioane <aioane.costin@yahoo.com>
2020-12-13 15:31:33 +0200

    First commit

---
+++ Server/src/main/java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
---
+++ client/src/main/java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
